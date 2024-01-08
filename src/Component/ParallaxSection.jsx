a76492bd7134ca0587045dba62ee84082eedeb5d
prithwirajpm <wt.prithwiraj@gmail.com>
2024-01-08 11:39:20 +0530

    Initial commit, canot complieted

---
+++ src/Component/ParallaxSection.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import "./Parallax.css";
+import HillOne from "../asset/hill1.png";
+import HillTwo from "../asset/hill2.png";
+import HillThree from "../asset/hill3.png";
+import HillFour from "../asset/hill4.png";
+import HillFive from "../asset/hill5.png";
+import Tree from "../asset/tree.png";
+import Leaf from "../asset/leaf.png";
+import Plant from "../asset/plant.png";
+
+function ParallaxSection() {
+  const [scrollValue, setScrollValue] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollValue(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []); // Empty dependency array to run the effect only once on mount
+
+  return (
+    <div>
+      <header>
+        <h2 className="logo">Logo</h2>
+        <nav className="navigator">
+          <a href="#" className="activate">
+            Home
+          </a>
+          <a href="#">About</a>
+          <a href="#">Services</a>
+          <a href="#">Contact</a>
+        </nav>
+      </header>
+      <section className="parallax">
+        <img
+          src={HillOne}
+          alt=""
+          srcSet=""
+          style={{ top: `${scrollValue * 1.5}px` }}
+          id="hill1"
+        />
+        <img src={HillTwo} alt="" srcSet="" id="hill2" />
+        <img src={HillThree} alt="" srcSet="" id="hill3" />
+        <img
+          src={HillFour}
+          alt=""
+          srcSet=""
+          style={{ left: `${scrollValue * -1.5}px` }}
+          id="hill4"
+        />
+        <img
+          src={HillFive}
+          alt=""
+          srcSet=""
+          style={{ left: `${scrollValue * 1.5}px` }}
+          id="hill5"
+        />
+        <h2 style={{ marginTop: `${scrollValue * 2.5}px` }} id="text">
+          Parallax Website
+        </h2>
+        <img src={Tree} alt="" srcSet="" id="tree" />
+        <img
+          src={Leaf}
+          alt=""
+          srcSet=""
+          style={{
+            top: `${scrollValue * -1.5}px`,
+            left: `${scrollValue * 1.5}px`,
+          }}
+          id="leaf"
+        />
+        <img src={Plant} alt="" srcSet="" id="plant" />
+      </section>
+    </div>
+  );
+}
+
+export default ParallaxSection;
